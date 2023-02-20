f1bfd591580fd9c26afc8ccb3c6157323eedb64d
unknown <laptopworld833@gmail.com>
2023-02-20 15:59:33 +0545

    first commit

---
+++ src/main/java/com/example/customermanagement/services/CustomerService.java
@@ -0,0 +1,28 @@
+package com.example.customermanagement.services;
+
+import com.example.customermanagement.Request.CustomerRequest;
+import com.example.customermanagement.entity.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    ResponseEntity<Object> saveCustomer(CustomerRequest customerRequest);
+
+    String updateCustomerStatus(int id,String customerRequest);
+    Page<Customer> getAllCustomer(Integer pageNumber,Integer pageSize);
+
+    ResponseEntity<Object> getCustomerById(int id);
+    Customer deleteCustomer(int id,boolean customerRequest);
+
+    ResponseEntity<Object> updateCustomerById(int id, CustomerRequest customerRequest);
+
+    ResponseEntity<Object> customerSearch(String name);
+    //Page<Customer> getAllCustomerBySorting(int page);
+    //List<Customer> searchCustomerById(int id);
+
+
+}
